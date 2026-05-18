98e73366e6150e447e7015d020880f0ad5b41f4d
A-Riad529 <ra3212007@gmail.com>
2026-05-18 21:31:55 +0300

    final to handel the runtime

--- Factorial.java
+++ Factorial.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class Factorial{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double num = sc.nextDouble();
+        long num = sc.nextLong();
         if (num < 1)
             return;
-        for(int i = (int) (num - 1); i >= 1; i--){
-        num *= i;
-            }
-        if(num % 2 == 0)
-            System.out.println("YES");
+        if (num == 1)
+            System.out.println("NO");
         else
-            System.out.println("\"NO\"");
+            System.out.println("YES");
     }
 }
