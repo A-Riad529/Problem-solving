91d37e5207030ee6d6a0919bcfe0b5250dfb7bb1
A-Riad529 <ra3212007@gmail.com>
2026-05-30 19:30:10 +0300

    mirged

---
+++ isDigit.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cctype>
+using namespace std;
+int main(){
+
+char check;
+cin>>check;
+int i = static_cast<int>(check);
+char upCase = (char)toupper(check);
+if('0' < i && i <= '9' || i == '0'){
+
+    cout<<"IS DIGIT";
+}
+else{
+    cout<<"ALPHA"<<"\n";
+    if(upCase == check){
+        cout<<"IS CAPITAL";
+    }
+    else{
+        cout<<"IS SMALL";
+    }
+}
+
+    return 0;
+}
